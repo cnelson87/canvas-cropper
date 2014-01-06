a67139d5e372b4040ac2c2d8bc0c65257aa495d4
Chris Nelson <cnelson87@gmail.com>
2014-01-06 12:01:10 -0800

    edits

--- src/scripts/Initialize.js
+++ src/scripts/Initialize.js
@@ -1,15 +1,6 @@
-/**
- * Application Initializer
- * 
- * @author Chris Nelson
- * @since  8.5.13
- */
 
 var Application = require('./Application');
 
 $(function() {
-
-	// Initialize Application
 	Application.initialize();
-
 });
